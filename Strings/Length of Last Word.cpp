4954da53f9c185f829879d8de50d113c5996e2b5
romamishra30 <roma.mishra@somaiya.edu>
2026-01-18 01:21:54 +0530

    DSA Practise

---
+++ Strings/Length of Last Word.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLastWord(string s){
+        int i = s.size() - 1;
+        int length = 0;
+
+        while(i>=0 && s[i] == ' '){
+            i--;
+        }
+        while(i>=0 && s[i] != ' '){
+            length++;
+            i--;
+        }
+        return length;
+    }
+};
