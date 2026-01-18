930031e584dc5ed996ec310e4a1aac5f9cc2dec7
romamishra30 <roma.mishra@somaiya.edu>
2026-01-18 23:40:10 +0530

    DSA Practise

---
+++ Strings/Find the Difference.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    char findTheDifference(string s, string t){
+        char ans = 0;
+
+        for(int i = 0; i<s.size(); i++){
+            ans ^= s[i];
+        }
+        for(int i = 0; i<t.size(); i++){
+            ans ^= t[i];
+        }
+        return ans;
+    }
+};
