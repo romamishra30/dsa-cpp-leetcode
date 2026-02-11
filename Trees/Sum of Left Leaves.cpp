4ac6d3d194f0575b258d5a5ed955c5dfb53df96c
romamishra30 <roma.mishra@somaiya.edu>
2026-02-12 00:36:08 +0530

    DSA Practice

---
+++ Trees/Sum of Left Leaves.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Definition for a binary tree node.
+  struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    bool isLeaf(TreeNode* root){
+      return root != nullptr &&
+             root->left == nullptr &&
+             root->right == nullptr;
+    }
+
+    int sumOfLeftLeaves(TreeNode* root){
+      if(root == nullptr) return 0;
+
+      int sum = 0;
+
+      if(isLeaf(root->left)){
+        sum += root->left->val;
+      }
+
+      sum += sumOfLeftLeaves(root->left);
+      sum += sumOfLeftLeaves(root->right);
+
+      return sum;
+    }
+};
